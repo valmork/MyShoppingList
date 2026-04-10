27f0c709e7bc9c4a84f5a8ece73788ce7c0f3428
anton <korolyov.anton0208@gmail.com>
2026-04-10 20:24:42 +0300

    Добавил data - слой

---
+++ app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.myshoppinglist.data
+
+import androidx.annotation.PluralsRes
+import com.example.myshoppinglist.domain.ShopItem
+import com.example.myshoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList
+    }
+}
