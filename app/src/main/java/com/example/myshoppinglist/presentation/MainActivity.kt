a242c5b480cdcce8a5d1adc78940280add27f0a8
anton <korolyov.anton0208@gmail.com>
2026-04-10 22:46:26 +0300

    Добавил presentation - слой

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.myshoppinglist.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.myshoppinglist.R
+import com.example.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +23,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivityTest", it.toString())
+        }
+        viewModel.getShopList()
+        viewModel.deleteShopItem(ShopItem("Item 2" , 2, true, id = 2))
     }
 }
