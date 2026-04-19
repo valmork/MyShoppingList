4fdd5eaffb576fc60f69b8f0f916bfaa6f0eb48f
anton <korolyov.anton0208@gmail.com>
2026-04-19 14:44:44 +0300

    Добавил ShopItemActivity и макет

---
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myshoppinglist.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myshoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_shop_item)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+            v.setPadding(
+                systemBars.left,
+                maxOf(systemBars.top, imeInsets.top),
+                systemBars.right,
+                maxOf(systemBars.bottom, imeInsets.bottom)
+            )
+            insets
+        }
+    }
+}
