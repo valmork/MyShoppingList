ebcaff0da3c95bf3cd9de422b3845970f821b38e
anton <korolyov.anton0208@gmail.com>
2026-04-21 19:03:53 +0300

    Исправил оповещение об ошибках ввода и пофиксил editShopItem, чтобы возвращались корректные поля

--- app/src/main/java/com/example/myshoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.myshoppinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myshoppinglist.data.ShopListRepositoryImpl
 import com.example.myshoppinglist.domain.AddShopItemUseCase
@@ -11,22 +13,44 @@ class ShopItemViewModel : ViewModel() {
 
     private val repository = ShopListRepositoryImpl
 
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Boolean>()
+    val shouldCloseScreen: LiveData<Boolean>
+        get() = _shouldCloseScreen
+
     private val getShopItemUseCase = GetShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
 
     fun getShopItem(shopItemId: Int) {
         val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
     /* Здесь 2 проблемы: id по умолчанию в конструкторе будет -1, в edit мы не найдем его,
-    * вторая проблема - по умолчанию ставится enabled = true */
+    * вторая проблема - по умолчанию ставится enabled = true
+    *
+    * Решение - создать и использовать LiveData, оттуда доставать старый объект и копировать его*/
     fun editShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
         if (validateInput(name, count)) {
-            val shopItem = ShopItem(name, count, true)
-            editShopItemUseCase.editShopItem(shopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
         }
     }
 
@@ -36,7 +60,9 @@ class ShopItemViewModel : ViewModel() {
         if (validateInput(name, count)) {
             val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
+
     }
 
     private fun parseName(inputName: String?): String {
@@ -45,16 +71,29 @@ class ShopItemViewModel : ViewModel() {
 
     private fun parseCount(inputCount: String?): Int  = inputCount?.trim()?.toIntOrNull() ?: 0
 
+    // Проверка правильности введенных значений в полях
     private fun validateInput(name: String, count: Int): Boolean {
         var isValid = true
         if (name.isBlank()) {
-            // TODO: show error input name
+            _errorInputName.value = true
             isValid = false
         }
         if (count <= 0) {
-            // TODO: show error input count
+            _errorInputCount.value = true
             isValid = false
         }
         return isValid
+    }
+
+    private fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    private fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = true
     }
 }
